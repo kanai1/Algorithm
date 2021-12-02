de4c6f51c41efc0e727369de46f8b2da27ebdb89
kanai <34500707+kanai1@users.noreply.github.com>
2021-12-02 14:57:52 +0900

    Update bigint.h
    bigint 곱셈연산자 구현 완료(주석 해제)

--- bigint/bigint.h
+++ bigint/bigint.h
@@ -24,7 +24,7 @@ public:
 
     friend bigint operator+(const bigint& num1, const bigint& num2);
     friend bigint operator-(const bigint& num1, const bigint& num2);
-    // friend bigint operator*(const bigint& num1, const bigint& num2);
+    friend bigint operator*(const bigint& num1, const bigint& num2);
     // friend bigint operator/(const bigint& num1, const bigint& num2);
     // friend bigint operator%(const bigint& num1, const bigint& num2);
 
@@ -35,7 +35,7 @@ public:
 
     friend bigint operator+=(bigint& num1, const bigint& num2) { return num1 = num1 + num2; }
     friend bigint operator-=(bigint& num1, const bigint& num2) { return num1 = num1 - num2; }
-    // friend bigint operator*=(bigint& num1, const bigint& num2) { return num1 = num1 * num2; }
+    friend bigint operator*=(bigint& num1, const bigint& num2) { return num1 = num1 * num2; }
     // friend bigint operator/=(bigint& num1, const bigint& num2) { return num1 = num1 / num2; }
     // friend bigint operator%=(bigint& num2, const bigint& num2) { return num1 = num1 % num2; }
 
