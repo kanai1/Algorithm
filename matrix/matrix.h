c8f0022b004885233a3bcf1e01dd701cf8f9c867
kanai <34500707+kanai1@users.noreply.github.com>
2021-12-10 16:03:50 +0900

    Update matrix.h
    행렬의 크기를 저장하기 위한 구조체 및 구조체연산(비교연산) 구현

--- matrix/matrix.h
+++ matrix/matrix.h
@@ -1,13 +1,30 @@
 #include <vector>
 
+struct matrix_size
+{
+    size_t row;
+    size_t col;
+
+    bool operator==(const matrix_size s) const
+    {
+        return this->row == s.row && this->col == s.col;
+    }
+
+    bool operator!=(const matrix_size s) const
+    {
+        return !(*this == s);
+    }
+};
+
 class matrix
 {
 private:
-    size_t row, col;
+    matrix_size m_size;
     long long** data;
 
 public:
     matrix(int col, int row);
+    matrix(matrix_size size);
     matrix(const void* other, int row, int col);
     matrix(const std::vector<std::vector<int>>& other);
     matrix(const matrix& other);
@@ -16,13 +33,18 @@ public:
 
     friend matrix operator*(const matrix& mt, const int num);
 
-    friend matrix operator+(const matrix& mt1, const matrix mt2);
-    friend matrix operator-(const matrix& mt1, const matrix mt2);
-    friend matrix operator*(const matrix& mt1, const matrix mt2);
+    friend matrix operator+(const matrix& mt1, const matrix& mt2);
+    friend matrix operator-(const matrix& mt1, const matrix& mt2);
+    friend matrix operator*(const matrix& mt1, const matrix& mt2);
 
     friend matrix transopse(const matrix& mt);
     friend long long det(const matrix& mt);
     friend long long trace(const matrix& mt);
 
+    matrix_size size() const { return m_size; }
+
     long long& element(int x, int y);
+
+private:
+    long long& at(int x, int y);
 };
